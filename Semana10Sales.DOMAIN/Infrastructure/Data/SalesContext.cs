cccfb707d82c681b3d02d71b3766d259a7510d8f
Luis Chang <luis.chang@iugo.com.uy>
2022-05-30 20:39:00 -0500

    Agregar archivos de proyecto.

---
+++ Semana10Sales.DOMAIN/Infrastructure/Data/SalesContext.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Semana10Sales.DOMAIN.Core.Entities;
+
+namespace Semana10Sales.DOMAIN.Infrastructure.Data
+{
+    public partial class SalesContext : DbContext
+    {
+        public SalesContext()
+        {
+        }
+
+        public SalesContext(DbContextOptions<SalesContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customer { get; set; } = null!;
+        public virtual DbSet<Order> Order { get; set; } = null!;
+        public virtual DbSet<OrderItem> OrderItem { get; set; } = null!;
+        public virtual DbSet<Product> Product { get; set; } = null!;
+        public virtual DbSet<Supplier> Supplier { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("server=DESKTOP-S1DROK0\\SQLEXPRESS;Database=Sales;Trusted_Connection=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.City).HasMaxLength(40);
+
+                entity.Property(e => e.Country).HasMaxLength(40);
+
+                entity.Property(e => e.FirstName).HasMaxLength(40);
+
+                entity.Property(e => e.LastName).HasMaxLength(40);
+
+                entity.Property(e => e.Phone).HasMaxLength(20);
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.Property(e => e.OrderDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.OrderNumber).HasMaxLength(10);
+
+                entity.Property(e => e.TotalAmount)
+                    .HasColumnType("decimal(12, 2)")
+                    .HasDefaultValueSql("((0))");
+
+                entity.HasOne(d => d.Customer)
+                    .WithMany(p => p.Order)
+                    .HasForeignKey(d => d.CustomerId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ORDER_REFERENCE_CUSTOMER");
+            });
+
+            modelBuilder.Entity<OrderItem>(entity =>
+            {
+                entity.Property(e => e.Quantity).HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.UnitPrice).HasColumnType("decimal(12, 2)");
+
+                entity.HasOne(d => d.Order)
+                    .WithMany(p => p.OrderItem)
+                    .HasForeignKey(d => d.OrderId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ORDERITE_REFERENCE_ORDER");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.OrderItem)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ORDERITE_REFERENCE_PRODUCT");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Package).HasMaxLength(30);
+
+                entity.Property(e => e.ProductName).HasMaxLength(50);
+
+                entity.Property(e => e.UnitPrice)
+                    .HasColumnType("decimal(12, 2)")
+                    .HasDefaultValueSql("((0))");
+
+                entity.HasOne(d => d.Supplier)
+                    .WithMany(p => p.Product)
+                    .HasForeignKey(d => d.SupplierId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_PRODUCT_REFERENCE_SUPPLIER");
+            });
+
+            modelBuilder.Entity<Supplier>(entity =>
+            {
+                entity.Property(e => e.City).HasMaxLength(40);
+
+                entity.Property(e => e.CompanyName).HasMaxLength(40);
+
+                entity.Property(e => e.ContactName).HasMaxLength(50);
+
+                entity.Property(e => e.ContactTitle).HasMaxLength(40);
+
+                entity.Property(e => e.Country).HasMaxLength(40);
+
+                entity.Property(e => e.Fax).HasMaxLength(30);
+
+                entity.Property(e => e.Phone).HasMaxLength(30);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Semana10Sales.DOMAIN/Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Semana10Sales.DOMAIN.Core.Entities;
+using Semana10Sales.DOMAIN.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana10Sales.DOMAIN.Infrastructure.Repositories
+{
+    public class CustomerRepository
+    {
+        private readonly SalesContext _context;
+
+        public CustomerRepository(SalesContext context)
+        {
+            _context = context;
+        }
+
+        //Get All Customers
+        public async Task<IEnumerable<Customer>> GetAll()
+        {
+            return await _context.Customer.ToListAsync();
+        }
+        //Get Customer by id
+        public async Task<Customer> GetById(int id)
+        {
+            return await _context.Customer.Where(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        //Get All Customers by Stored Procedure
+        public IEnumerable<Customer> GetAllBySP()
+        {
+            return _context.Customer.FromSqlInterpolated($"EXECUTE dbo.GET_ALL_CUSTOMER").ToList();
+        }
+
+        //Insert Customer
+        public async Task<bool> Insert(Customer customer)
+        {
+            await _context.Customer.AddAsync(customer);
+            int rows = await _context.SaveChangesAsync();
+            return rows>0;
+        }
+
+        //Update Customer
+        public async Task<bool> Update(Customer customer)
+        {
+            _context.Customer.Update(customer);
+            int rows = await _context.SaveChangesAsync();
+            return rows > 0;
+        }
+        //Delete Customer
+        public async Task<bool> Delete(int id)
+        {
+            var customer = await _context.Customer.FindAsync(id);
+            //Validate is null
+            if (customer == null)
+                return false;
+
+            _context.Customer.Remove(customer);
+            int rows = await _context.SaveChangesAsync();
+            return rows > 0;
+
+        }
+    }
+}
